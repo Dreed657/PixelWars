6c431112190ddf9e3dbe4d5ae9182a5b291c8054
Stoyan Lazarov <ninjakiviWTF@gmail.com>
2022-04-22 01:17:19 +0300

    Add user interaction

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState, useRef, useEffect } from 'react';
 import './App.css';
 
 const App = () => {
+    const [selectedColor, setSelectedColor] = useState(50);
     const [time, setTime] = useState(new Date().getTime());
     const [canvas, setCanvas] = useState();
     const ws = useRef(null);
@@ -21,7 +22,7 @@ const App = () => {
     useEffect(() => {
         if (!ws.current) return;
 
-        ws.current.onmessage = (e) => {
+        ws.current.onmessage = e => {
             const response = JSON.parse(e.data.toString());
 
             // console.log('message', response);
@@ -63,21 +64,42 @@ const App = () => {
         };
     }, [canvas]); // this is triggered everytime time state is updated
 
+    const selectColorClick = (colorIndex) => {
+        setSelectedColor(colorIndex);
+    }
+
+    const cellClick = (x, y) => {
+        if (selectedColor == 50) { return; }
+
+        let payload = {x, y, color: selectedColor};
+
+        // console.log(payload);
+
+        ws.current.send(
+            JSON.stringify({
+                type: 'updateCell',
+                data: {
+                    updatedCell: payload
+                }
+            })
+        );
+    }
+
     return (
         <div className="container">
-            <h1>ARE WE THERE YET</h1>
-            <h6>{new Date().getTime()}</h6>
-            <h6>{time}</h6>
+            <h1>PIXELWARS</h1>
+            <br />
             <span className="border-box">
                 {canvas ? (
-                    canvas.map((row, i) => {
+                    canvas.map((row, x) => {
                         return (
-                            <div className="row" key={i}>
+                            <div className="row" key={x}>
                                 {row.map((cell, y) => {
                                     return (
                                         <div
                                             className={'color-' + cell.color}
                                             key={y}
+                                            onClick={() => cellClick(x, y)}
                                         ></div>
                                     );
                                 })}
@@ -88,6 +110,16 @@ const App = () => {
                     <p>Loading...</p>
                 )}
             </span>
+            <div className="border-box colors">
+                <div className="row">
+                    <div className={`color-0 ${selectedColor == 0 ? 'selected' : ''}`} onClick={() => selectColorClick(0)}></div>
+                    <div className={`color-1 ${selectedColor == 1 ? 'selected' : ''}`} onClick={() => selectColorClick(1)}></div>
+                    <div className={`color-2 ${selectedColor == 2 ? 'selected' : ''}`} onClick={() => selectColorClick(2)}></div>
+                    <div className={`color-3 ${selectedColor == 3 ? 'selected' : ''}`} onClick={() => selectColorClick(3)}></div>
+                    <div className={`color-4 ${selectedColor == 4 ? 'selected' : ''}`} onClick={() => selectColorClick(4)}></div>
+                    <div className={`color-5 ${selectedColor == 5 ? 'selected' : ''}`} onClick={() => selectColorClick(5)}></div>
+                </div>
+            </div>
         </div>
     );
 };
